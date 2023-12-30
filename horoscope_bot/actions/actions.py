a811cc2efcfb843b2c67358afc4e3e1c2c663bab
Henrique F. Paes <henrique.paes@gmail.com>
2023-12-29 23:06:11 -0300

    Update Horoscope_bot project

--- horoscope_bot/actions/actions.py
+++ horoscope_bot/actions/actions.py
@@ -1,27 +1,42 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+import requests
 
+class GetTodaysHoroscope(Action):
+    def name(self) -> Text:
+        return "get_todays_horoscope"
 
-# This is a simple example for a custom action which utters "Hello World!"
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+        user_horoscope_sign = tracker.get_slot('horoscope_sign')
+        base_url = 'https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily?sign={sign}&day={day}'
+        url = base_url.format(**{'day': "today", 'sign': user_horoscope_sign})
+
+        res = requests.get(url)
+        todays_horoscope = res.json()['horoscope_data']
+        response = "Your today's horoscope:\n{}".format(todays_horoscope)
+
+        dispatcher.utter_message(response)
+
+        return [SlotSet("horoscope_sign", user_horoscope_sign)]
+
+class SubscriberUser(Action):
+    def name(self):
+        return "subscriber_user"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        subscribe = tracker.get_slot('subscribe')
+
+        if subscribe == 'True':
+            response = "You're successfully subscribed"
+        if subscribe == 'False':
+            response =  "You're successfully unsubscribed"
+
+        dispatcher.utter_message(response)
+        return [SlotSet("subscribe", subscribe)]
